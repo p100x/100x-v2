0c7f292777bc35b26547c72e14b55f87c44abf87
p100x <maximiliankrehl@icloud.com>
2024-09-23 01:01:40 +0200

    push

--- src/components/LoginOverlay.js
+++ src/components/LoginOverlay.js
@@ -1,10 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useAuth } from '../contexts/AuthContext';
 
 const LoginOverlay = () => {
   const [email, setEmail] = useState('');
   const [error, setError] = useState(null);
   const { signIn, user } = useAuth();
+  const [typedText, setTypedText] = useState('');
+  const fullText = '100X';
+
+  useEffect(() => {
+    if (typedText.length < fullText.length) {
+      const timeout = setTimeout(() => {
+        setTypedText(fullText.slice(0, typedText.length + 1));
+      }, 200); // Adjust typing speed here
+      return () => clearTimeout(timeout);
+    }
+  }, [typedText]);
 
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -24,8 +35,17 @@ const LoginOverlay = () => {
   return (
     <div className="login-overlay">
       <div className="login-form-container">
+        <div className="app-name-container">
+          <div className="logo-version-container">
+            <div className="app-name">
+              {typedText}
+              <span className="cursor">|</span>
+            </div>
+            <div className="version-number">v0.0.9</div>
+          </div>
+          <div className="alpha-pill">alpha</div>
+        </div>
         <form onSubmit={handleLogin}>
-          <h2>Login</h2>
           <input
             type="email"
             placeholder="Email"
