95d32dd60d863f73cdf37efe17ffad3afbfe0f23
Maximilian <maximilian@Maximilians-MacBook-Pro-2.fritz.box>
2024-10-09 19:45:38 +0200

    fixes

--- src/components/EarningsCalendar.js
+++ src/components/EarningsCalendar.js
@@ -28,11 +28,17 @@ const EarningsCalendar = ({ isOpen, setIsOpen }) => {
   useEffect(() => {
     if (isOpen) {
       document.body.style.overflow = 'hidden';
+      document.body.style.position = 'fixed';
+      document.body.style.width = '100%';
     } else {
-      document.body.style.overflow = 'unset';
+      document.body.style.overflow = '';
+      document.body.style.position = '';
+      document.body.style.width = '';
     }
     return () => {
-      document.body.style.overflow = 'unset';
+      document.body.style.overflow = '';
+      document.body.style.position = '';
+      document.body.style.width = '';
     };
   }, [isOpen]);
 
